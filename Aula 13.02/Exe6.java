ca4076c31bf4619d22f7a676fff82fb04cfe248a
thiagopoles <polesellothiago14@gmail.com>
2026-02-16 16:43:32 -0300

    commit java script

---
+++ Aula 13.02/Exe6.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Exe6 {
+
+	public static void main(String[] args) {
+		String nome;
+		float metros;
+		float Kg;
+		float densidade;
+		Scanner teclado = new Scanner(System.in);
+		System.out.print("Digite o seu nome: ");
+		nome = teclado.next();
+		System.out.print("Digite a metros: ");
+		metros = teclado.nextFloat();
+		System.out.print("Digite o Kg: ");
+		Kg = teclado.nextFloat();
+		
+		densidade = Kg / metros;
+
+		System.out.println("Seu nome: " + nome);
+		System.out.println("Densidade: " + densidade);
+	}
+}
